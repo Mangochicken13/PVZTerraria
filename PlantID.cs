a6dd312b31eb17d26f0ba4ea893da16f78b4175e
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2023-11-07 14:22:53 +0800

    Update PlantID.cs
    Whitespace removal, + adding the search present in the tmod ID classes

--- PlantID.cs
+++ PlantID.cs
@@ -1,21 +1,20 @@
-﻿namespace PlantsVsZombies
+﻿using ReLogic.Reflection;
+
+namespace PlantsVsZombies
 {
     public class PlantID
     {
         // Note to self: Try and find a way to automatically populate this according to some order
 
-        public const short Count = 5;
-
         public const short Null = 0;
 
-
-
         public const short Sunflower = 1;
-
         public const short Peashooter = 2;
-
         public const short Rotobaga = 3;
-
         public const short BananaLauncher = 4;
+
+        public const short Count = 5;
+
+        public static readonly IdDictionary Search = IdDictionary.Create<PlantID, short>();
     }
 }
