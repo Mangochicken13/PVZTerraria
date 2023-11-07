7852362cd7af09593dd316633b3a2ac9403ea6ce
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2023-11-07 14:27:02 +0800

    Move dev keybinds to debug builds only

--- Common/Players/KeybindPlayer.cs
+++ Common/Players/KeybindPlayer.cs
@@ -12,17 +12,16 @@ namespace PlantsVsZombies.Common.Players
         //both of these are dev features, and won't stick around for the full mod release
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
+#if DEBUG
             if (KeybindSystem.Dev_ToggleUI.JustPressed)
             {
                 if (ModContent.GetInstance<SunUISystem>().MyInterface?.CurrentState == null)
                 {
                     ModContent.GetInstance<SunUISystem>().ShowMyUI();
-                    Main.NewText("Ui Shown");
                 }
                 else if (ModContent.GetInstance<SunUISystem>().MyInterface?.CurrentState != null)
                 {
                     ModContent.GetInstance<SunUISystem>().HideMyUI();
-                    Main.NewText("Ui Hidden");
                 }
             }
 
@@ -31,6 +30,7 @@ namespace PlantsVsZombies.Common.Players
                 var Sun = Player.GetModPlayer<Sun>();
                 Sun.CurrentSun += 50;
             }
+#endif
         }
     }
 }
--- Common/Systems/KeybindSystem.cs
+++ Common/Systems/KeybindSystem.cs
@@ -12,13 +12,17 @@ namespace PlantsVsZombies.Common.Systems
         public static ModKeybind Dev_ExtraSun { get; set; }
         public override void Load()
         {
+#if DEBUG
             Dev_ToggleUI = KeybindLoader.RegisterKeybind(Mod, "Toggle UI", "U");
             Dev_ExtraSun = KeybindLoader.RegisterKeybind(Mod, "Add 50 sun", "=");
+#endif
         }
         public override void Unload()
         {
+#if DEBUG
             Dev_ToggleUI = null;
             Dev_ExtraSun = null;
+#endif
         }
     }
 }
